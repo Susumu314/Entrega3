16aa5cad7bada6b3f12ff644409fc45764e68986
Susumu314 <hugo.susumu@gmail.com>
2021-07-20 12:52:22 -0200

    Gerenciamento da tabela Paciente_exame_amostra pronto
    falta só consertar o change_agregadoPEA que está pronto, só vai faltar a parte do ep que lida com o postgresql

--- ep3/gerenciamentoPEA/urls.py
+++ ep3/gerenciamentoPEA/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    
     path('pacientes/', views.pacientes, name='pacientes'),
     path('pacientes/add_paciente/', views.add_paciente, name='add_paciente'),
     path('pacientes/save_paciente/', views.save_paciente, name='save_paciente'),
@@ -11,16 +12,25 @@ urlpatterns = [
     path('pacientes/<int:id_paciente>/delete_paciente', views.delete_paciente, name='delete_paciente'),
     path('pacientes/<int:id_paciente>/', views.paciente_detail, name='p_detalhes'),
     path('pacientes/<int:id_paciente>/exames/', views.exames_do_paciente, name='p_exames'),
+
     path('exames/', views.exames, name='exames'),
     path('exames/<int:id_exame>/', views.exame_detail, name='e_detalhes'),
     path('exames/adiciona_exame/', views.add_exame, name='add_exame'),
     path('exames/save_exame/', views.save_exame, name='save_exame'),
     path('exames/<int:id_exame>/change_exame', views.change_exame, name='change_exame'),
     path('exames/<int:id_exame>/delete_exame', views.delete_exame, name='delete_exame'),
+
     path('amostras/', views.amostras, name='amostras'),
     path('amostras/add_amostra/', views.add_amostra, name='add_amostra'),
     path('amostras/save_amostra/', views.save_amostra, name='save_amostra'),
     path('amostras/<int:id_amostra>/change_amostra', views.change_amostra, name='change_amostra'),
     path('amostras/<int:id_amostra>/delete_amostra', views.delete_amostra, name='delete_amostra'),
     path('amostras/<int:id_amostra>/', views.amostra_detail, name='a_detalhes'),
+
+    path('agregadoPEAs/', views.agregadoPEAs, name='agregadoPEAs'),
+    path('agregadoPEAs/<int:id_agregadoPEA>/', views.agregadoPEA_detail, name='ag_detalhes'),
+    path('agregadoPEAs/adiciona_agregadoPEA/', views.add_agregadoPEA, name='add_agregadoPEA'),
+    path('agregadoPEAs/save_agregadoPEA/', views.save_agregadoPEA, name='save_agregadoPEA'),
+    path('agregadoPEAs/<int:id_agregadoPEA>/change_agregadoPEA', views.change_agregadoPEA, name='change_agregadoPEA'),
+    path('agregadoPEAs/<int:id_agregadoPEA>/delete_agregadoPEA', views.delete_agregadoPEA, name='delete_agregadoPEA'),
 ]
--- ep3/gerenciamentoPEA/views.py
+++ ep3/gerenciamentoPEA/views.py
@@ -3,11 +3,14 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from .models import Paciente, Paciente_exame_amostra, Exame, Amostra
 from django.template import loader
 from django.urls import reverse
+from datetime import datetime
 # Create your views here.
 
 def index(request):
     template = loader.get_template('gerenciamentoPEA/index.html')
     return render(request, 'gerenciamentoPEA/index.html', {})
+
+# Paciente
 
 def pacientes(request):
     paciente_list = Paciente.objects.order_by('cpf')
@@ -21,7 +24,7 @@ def add_paciente(request):
     return render(request, 'gerenciamentoPEA/add_paciente.html', {})
 
 def save_paciente(request):
-    try:#mudar aqui para nao aceitar id que ja existe
+    try:
         p = Paciente(id_paciente=request.POST['id_paciente'], cpf=request.POST['cpf'], nome=request.POST['nome'], endereco=request.POST['endereco'], nascimento=request.POST['nascimento'])
     except (KeyError):
         # Redisplay the question voting form.
@@ -43,6 +46,8 @@ def delete_paciente(request, id_paciente):
     Paciente.objects.filter(id_paciente=id_paciente).delete()
     return render(request, 'gerenciamentoPEA/delete_paciente.html', {})
 
+# Exames
+
 def exames(request):
     exame_list = Exame.objects.order_by('id_exame')
     return render(request, 'gerenciamentoPEA/exames.html', {'exame_list': exame_list})
@@ -60,7 +65,7 @@ def exames_do_paciente(request, id_paciente):
     return HttpResponse(response % id_paciente)
 
 def save_exame(request):
-    try:#mudar aqui para nao aceitar id que ja existe
+    try:
         p = get_object_or_404(Paciente, pk=request.POST['id_paciente'])
         try:
             Exame.objects.filter(id_exame=request.POST['id_exame']).delete()
@@ -88,6 +93,8 @@ def delete_exame(request, id_exame):
     Exame.objects.filter(id_exame=id_exame).delete()
     return render(request, 'gerenciamentoPEA/delete_exame.html', {})
 
+# Amostra
+
 def amostras(request):
     amostra_list = Amostra.objects.order_by('id_amostra')
     return render(request, 'gerenciamentoPEA/amostras.html', {'amostra_list': amostra_list})
@@ -100,7 +107,7 @@ def add_amostra(request):
     return render(request, 'gerenciamentoPEA/add_amostra.html', {})
 
 def save_amostra(request):
-    try:#mudar aqui para nao aceitar id que ja existe
+    try:
         a = Amostra(id_amostra=request.POST['id_amostra'], codigo_amostra=request.POST['codigo_amostra'], metodo_de_coleta=request.POST['metodo_de_coleta'], material=request.POST['material'])
     except (KeyError):
         # Redisplay the question voting form.
@@ -121,3 +128,47 @@ def change_amostra(request, id_amostra):
 def delete_amostra(request, id_amostra):
     Amostra.objects.filter(id_amostra=id_amostra).delete()
     return render(request, 'gerenciamentoPEA/delete_amostra.html', {})
+
+# Agregado Paciente_exame_amostra
+
+def agregadoPEAs(request):
+    agregadoPEA_list = Paciente_exame_amostra.objects.order_by('id')
+    return render(request, 'gerenciamentoPEA/agregadoPEAs.html', {'agregadoPEA_list': agregadoPEA_list})
+
+def add_agregadoPEA(request):
+    paciente_list = Paciente.objects.order_by('id_paciente')
+    exame_list = Exame.objects.order_by('id_exame')
+    amostra_list = Amostra.objects.order_by('id_amostra')
+    return render(request, 'gerenciamentoPEA/add_agregadoPEA.html', {'paciente_list': paciente_list,'amostra_list': amostra_list,'exame_list': exame_list})
+
+def agregadoPEA_detail(request, id_agregadoPEA):
+    agregadoPEA = get_object_or_404(Paciente_exame_amostra, pk=id_agregadoPEA)
+    return render(request, 'gerenciamentoPEA/agregadoPEA_detalhes.html', {'agregadoPEA': agregadoPEA})
+
+def save_agregadoPEA(request):
+    paciente = get_object_or_404(Paciente, pk=request.POST['id_paciente'])
+    exame = get_object_or_404(Exame, pk=request.POST['id_exame'])
+    amostra = get_object_or_404(Amostra, pk=request.POST['id_amostra'])
+    try:
+        ag = Paciente_exame_amostra(id_paciente=paciente, id_exame=exame, id_amostra=amostra, data_de_realizacao=(request.POST['data_de_realizacao_0'] + ' ' + request.POST['data_de_realizacao_1']), data_de_solicitacao=(request.POST['data_de_solicitacao_0']+' '+request.POST['data_de_solicitacao_1']))
+    except (KeyError):
+        return render(request, 'gerenciamentoPEA/add_agregadoPEA.html', {
+            'error_message': "Deu erro para cadastrar o Paciente_exame_amostra",
+        })
+    else:
+        ag.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return render(request, 'gerenciamentoPEA/save_agregadoPEA.html', {'paciente': ag})
+    
+def change_agregadoPEA(request, id_agregadoPEA):
+    paciente_list = Paciente.objects.order_by('id_paciente')
+    exame_list = Exame.objects.order_by('id_exame')
+    amostra_list = Amostra.objects.order_by('id_amostra')
+    agregadoPEA = get_object_or_404(Paciente_exame_amostra, pk=id_agregadoPEA)
+    return render(request, 'gerenciamentoPEA/change_agregadoPEA.html', {'agregadoPEA':agregadoPEA, 'paciente_list': paciente_list,'amostra_list': amostra_list,'exame_list': exame_list})
+
+def delete_agregadoPEA(request, id_agregadoPEA):
+    Paciente_exame_amostra.objects.filter(id=id_agregadoPEA).delete()
+    return render(request, 'gerenciamentoPEA/delete_agregadoPEA.html', {})
